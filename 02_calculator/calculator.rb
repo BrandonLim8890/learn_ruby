6e29656d39b740f7fe77ddd0935e2e556ce3d999
Brandon Lim <brandonlim8890@gmail.com>
2019-10-05 23:32:54 +0800

    Worked on first 3 ruby files

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
 #write your code here
+def add num1, num2
+    num1 + num2
+end
+
+def subtract num1, num2
+    num1 - num2
+end
+
+def sum array
+    total = 0 
+    array.each do |value|
+        total += value
+    end
+    total
+end
+
+def mulitply num1, num2
+    num1 * num2
+end
+
+def power base, power
+    base ** power
+end
+
+def factorial num
+    if num <= 1
+        return 1
+    else
+        num * factorial(num - 1)
+    end
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,39 @@
 #write your code here
+def echo input
+    input
+end
+
+def shout input
+    input.upcase
+end
+
+def repeat (input, repeat = 2)
+    array = []
+    repeat.times do
+        array.push(input)
+    end
+    array.join(" ")
+end
+
+def start_of_word (word, position)
+    ans = ""
+    for i in 1..position do
+        ans += word[i - 1]
+    end
+    ans
+end
+
+def first_word words
+    array = words.split()
+    array[0]
+end
+
+def titleize word
+    array = word.split()
+    for i in 0..array.length - 1 do
+        if array[i] != "and" and array[i] != "the" and array[i] != "over" or i == 0
+            array[i][0] = array[i][0].upcase
+        end
+    end
+    array.join(" ")
+end
